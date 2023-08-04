a3b11433333fd70928354b4c3cfb3546c28785cc
martinsendati <martin.barrionuevo@sendati.com>
2023-08-04 13:15:33 -0300

    nueva-app

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 var images = [
-  "https://i.imgur.com/2Z8qEvC.png",
-  "https://i.imgur.com/2Xntz2P.jpg",
-  "https://i.imgur.com/pIGcI2d.jpg",
-  "https://i.imgur.com/n5lNz3z.jpg",
-  "https://i.imgur.com/tup8Ocu.jpg",
-  "https://i.imgur.com/yjpOvlM.jpg",
-  "https://i.imgur.com/akNhnrh.jpg",
-  "https://i.imgur.com/eqpDlh5.jpg",
-  "https://i.imgur.com/ARqnwlQ.jpg",
-  "https://i.imgur.com/2lJ6xLl.jpg",
-  "https://i.imgur.com/kGQUUTw.jpg",
-  "https://i.imgur.com/o4LuRu7.jpg",
-  "https://i.imgur.com/CnDPSpV.png",
-  "https://i.imgur.com/E0s8TW6.png",
-  "https://i.imgur.com/xSmJv0k.png",
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fproyectoviajero.com%2Fmetodos-de-tortura-mas-crueles%2F&psig=AOvVaw221MLWJzEwmXHBnIqq9pFZ&ust=1691251703538000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMiKxtuxw4ADFQAAAAAdAAAAABAD",
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ovhcloud.com%2Fes%2Fpublic-cloud%2Fkubernetes%2Fkubernetes-load-balancer%2F&psig=AOvVaw0DK3xhXl4l8Fg17wQF0LQT&ust=1691251743620000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCJ9u2xw4ADFQAAAAAdAAAAABAD",
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.influxdata.com%2Fintegration%2Fgrafana%2F&psig=AOvVaw2RwcuOX4gWLz6RZL1TLnGl&ust=1691251819582000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDsr5Kyw4ADFQAAAAAdAAAAABAD",
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Felcomercio.pe%2Fmundial%2Ffrancia%2Fgol-de-mbappe-de-penal-para-el-3-3-de-francia-vs-argentina-por-la-final-del-mundial-2022-ver-goles-de-mbappe-hoy-video-rmmd-dtbn-noticia%2F&psig=AOvVaw3Qft-87NNV4IWdLOxFDu7q&ust=1691251850020000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCICE7KWyw4ADFQAAAAAdAAAAABAD",
 ];
 
 function getRandomImage() {
